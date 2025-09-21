da112d4d000db19f043e47ccb0052a5a90fa9fad
aaryyya <aryampatil2005@gmail.com>
2025-09-21 23:20:53 +0530

    Spring Boot WebApp

---
+++ src/main/java/com/example/myWebApp/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.myWebApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String greet(){
+        System.out.println("Hi World Web App");
+        return "Welcome to my Arya's Web Page!";
+    }
+}
--- src/main/java/com/example/myWebApp/MyWebAppApplication.java
+++ src/main/java/com/example/myWebApp/MyWebAppApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyWebAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyWebAppApplication.class, args);
+
+        SpringApplication.run(MyWebAppApplication.class, args);
 	}
 
 }
