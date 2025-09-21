41904298fdbb8b69ce94dd58de5036839618a787
aaryyya <aryampatil2005@gmail.com>
2025-09-21 23:44:37 +0530

    Web Page

--- src/main/java/com/example/myWebApp/HomeController.java
+++ src/main/java/com/example/myWebApp/HomeController.java
@@ -6,13 +6,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Controller
+//@Controller
 public class HomeController {
 
     @RequestMapping("/")
-    @ResponseBody
+//    @ResponseBody
     public String greet(){
         System.out.println("Hi World Web App");
         return "Welcome to my Arya's Web Page!";
+    }
+    @RequestMapping("/about")
+//    @ResponseBody
+    public String about(){
+        return "This is ABOUT page";
     }
 }
