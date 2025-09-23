bfaf4d59c076c565401cdef087c02bf9dc038ff1
aaryyya <aryampatil2005@gmail.com>
2025-09-23 23:26:33 +0530

    HTTP methods Delete and Updating Done PUT and DELETE

--- src/main/java/com/example/myWebApp/controller/ProductController.java
+++ src/main/java/com/example/myWebApp/controller/ProductController.java
@@ -3,20 +3,43 @@ package com.example.myWebApp.controller;
 import com.example.myWebApp.model.Product;
 import com.example.myWebApp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 @RestController
 public class ProductController {
     @Autowired
     ProductService service;
-    @RequestMapping("/products")
+
+//    @RequestMapping(value = "/products",method = RequestMethod.GET)
+    @GetMapping("/products")
     public List<Product> getProducts(){
         return service.getProducts();
     }
 
-    public Product getProductById(int prodId){
+    @GetMapping("/products/{prodId}")
+//    @RequestMapping("/products/{prodId}")
+    public Product getProductById(@PathVariable int prodId){
         return service.getProductById(prodId);
+    }
+
+    @PostMapping("/products")
+//    @RequestMapping("/products")
+    public void addProduct(@RequestBody Product prod){
+        System.out.println(prod);
+        service.addProduct(prod);
+    }
+
+    @PutMapping("/products")
+    public void updateProduct(@RequestBody Product prod){
+        System.out.println("update is here");
+        service.updateProduct(prod);
+    }
+
+    @DeleteMapping("/products/{prodId}")
+    public void deleteProduct(@PathVariable int prodId){
+        service.deleteProduct(prodId);
+
     }
 }
--- src/main/java/com/example/myWebApp/service/ProductService.java
+++ src/main/java/com/example/myWebApp/service/ProductService.java
@@ -2,21 +2,25 @@ package com.example.myWebApp.service;
 
 import com.example.myWebApp.model.Product;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 @Service
 public class ProductService {
-    List<Product>products= Arrays.asList(new Product(101,"iphone",5000),
+    List<Product>products= new ArrayList<>(Arrays.asList(new Product(101,"iphone",5000),
     new Product(102,"wife phone00",5020),
-    new Product(103,"meowphone",4560));
-    public List<Product> getProducts(){
+    new Product(103,"meowphone",4560)));
 
+    public List<Product> getProducts(){
         return products;
     }
+
     @RequestMapping("/products/{prodId}")
-    public Product getProductById(int prodId){
+    public Product getProductById(@PathVariable int prodId){
         for(Product i: products)
         {
             if(i.getProdId() == prodId)
@@ -25,5 +29,31 @@ public class ProductService {
             }
         }
         return null;
+        /*return products.stream().filter(p->p.getProdId()==prodId).findFirst().orElse(new Product(100,"NewIphones",12);*/
     }
+
+    public  void addProduct(Product prod){
+        products.add(prod);
+    }
+
+    public void updateProduct(Product prod) {
+        int index=0;
+        for (int i = 0; i < products.size(); i++) {
+            if(products.get(i).getProdId()==prod.getProdId()){
+                index=i;
+            }
+        }
+        products.set(index,prod);
+    }
+
+    public void deleteProduct(int prodId){
+        int index=0;
+        for(int i=0;i<products.size();i++){
+            if(products.get(i).getProdId()==prodId){
+                index=i;
+            }
+        }
+        products.remove(index);
+    }
+
 }
